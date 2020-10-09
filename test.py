0ecf3a33f9bb1013afb76e08fcca9cb13c6964cd
marmys19 <mmuskovets@gmail.com>
2020-10-09 15:56:16 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+def sum(*params):
+    result = 0
+    for n in params:
+        result +=n
+    return result
+
+sum1 = sum(1,2,3,4,5,6,7,8,9)
+sum2 = sum(11,12,13,14,15,16,17,18,19)
+print(sum1)
+print(sum2)
